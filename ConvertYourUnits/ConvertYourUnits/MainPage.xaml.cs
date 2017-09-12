1606617f673d5a99221998980a165a27261160ca
Safonov85 <safonov85@yahoo.com>
2017-09-12 21:31:07 +0200

    First commit

---
+++ ConvertYourUnits/ConvertYourUnits/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ConvertYourUnits
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+			//picker.SelectedItem = "Kg to Lbs";
+			picker.Items.Add("Kg to Lbs");
+			picker.Items.Add("Celcius to Farhrenheit");
+			picker.Items.Add("Meters to Feet");
+			picker.Items.Add("Kg to Lbs");
+			picker.SelectedIndex = 3;
+		}
+
+		private void picker_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void sliderMain_ValueChanged(object sender, ValueChangedEventArgs e)
+		{
+			labelMain.Text = sliderMain.Value.ToString() + " Kg" + " = " +
+				ConvertKgToPounds(sliderMain.Value).ToString("##.#") + " lbs";
+		}
+
+		double ConvertKgToPounds(double kg)
+		{
+			double lbs = 2.204623;
+			lbs = kg * lbs;
+			return lbs;
+		}
+	}
+}
