53e096bdceb699e23b458d9b71f18d4cec2ea47a
Safonov85 <safonov85@yahoo.com>
2017-09-15 18:23:45 +0200

    Conversions added

--- ConvertYourUnits/ConvertYourUnits.Android/MainActivity.cs
+++ ConvertYourUnits/ConvertYourUnits.Android/MainActivity.cs
@@ -9,7 +9,8 @@ using Android.OS;
 
 namespace ConvertYourUnits.Droid
 {
-	[Activity(Label = "ConvertYourUnits", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity(Label = "ConvertYourUnits", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+		ScreenOrientation = ScreenOrientation.Portrait)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate(Bundle bundle)
--- ConvertYourUnits/ConvertYourUnits/MainPage.xaml.cs
+++ ConvertYourUnits/ConvertYourUnits/MainPage.xaml.cs
@@ -17,19 +17,76 @@ namespace ConvertYourUnits
 			picker.Items.Add("Kg to Lbs");
 			picker.Items.Add("Celcius to Farhrenheit");
 			picker.Items.Add("Meters to Feet");
-			picker.Items.Add("Kg to Lbs");
-			picker.SelectedIndex = 3;
+			picker.Items.Add("Km to Miles");
+			picker.SelectedIndex = 0;
+			
 		}
 
 		private void picker_SelectedIndexChanged(object sender, EventArgs e)
 		{
-			
+			if (picker.SelectedIndex == 0)
+			{
+				sliderMain.Minimum = 0;
+				sliderMain.Maximum = 100;
+				sliderMain.Value = 0;
+				labelMain.Text = sliderMain.Value.ToString("##.#") + " Kg" + " = " +
+				ConvertKgToPounds(sliderMain.Value).ToString("##.#") + " lbs";
+			}
+			else if (picker.SelectedIndex == 1)
+			{
+				sliderMain.Minimum = -100;
+				sliderMain.Maximum = 100;
+				sliderMain.Value = 0;
+				labelMain.Text = sliderMain.Value.ToString("##.#") + " °C" + " = " +
+					CelciusToFarenheit(sliderMain.Value).ToString("##.#") + " °F";
+			}
+			else if (picker.SelectedIndex == 2)
+			{
+				sliderMain.Minimum = 0;
+				sliderMain.Maximum = 100;
+				sliderMain.Value = 0;
+				labelMain.Text = sliderMain.Value.ToString("##.#") + " Meters" + " = " +
+				ConvertMetersToFeet(sliderMain.Value).ToString("##.#") + " Feet";
+			}
+			else if (picker.SelectedIndex == 3)
+			{
+				sliderMain.Minimum = 0;
+				sliderMain.Maximum = 1000;
+				sliderMain.Value = 0;
+				labelMain.Text = sliderMain.Value.ToString("##.#") + " Km" + " = " +
+				ConvertKmToMiles(sliderMain.Value).ToString("##.#") + " Miles";
+			}
 		}
 
 		private void sliderMain_ValueChanged(object sender, ValueChangedEventArgs e)
 		{
-			labelMain.Text = sliderMain.Value.ToString() + " Kg" + " = " +
+			if(picker.SelectedIndex == 0)
+			{
+
+				labelMain.Text = sliderMain.Value.ToString("##.#") + " Kg" + " = " +
 				ConvertKgToPounds(sliderMain.Value).ToString("##.#") + " lbs";
+			}
+			else if(picker.SelectedIndex == 1)
+			{
+				labelMain.Text = sliderMain.Value.ToString("##.#") + " °C" + " = " +
+					CelciusToFarenheit(sliderMain.Value).ToString("##.#") + " °F";
+			}
+			else if (picker.SelectedIndex == 2)
+			{
+				labelMain.Text = sliderMain.Value.ToString("##.#") + " Meters" + " = " +
+				ConvertMetersToFeet(sliderMain.Value).ToString("##.#") + " Feet";
+			}
+			else if (picker.SelectedIndex == 3)
+			{
+				labelMain.Text = sliderMain.Value.ToString("##.#") + " Km" + " = " +
+				ConvertKmToMiles(sliderMain.Value).ToString("##.#") + " Miles";
+			}
+		}
+
+		double CelciusToFarenheit(double cel)
+		{
+			double faren = (cel * 9 / 5) + 32;
+			return faren;
 		}
 
 		double ConvertKgToPounds(double kg)
@@ -37,6 +94,20 @@ namespace ConvertYourUnits
 			double lbs = 2.204623;
 			lbs = kg * lbs;
 			return lbs;
+		}
+
+		double ConvertMetersToFeet(double meters)
+		{
+			double feet = 3.280839895;
+			feet = meters * feet;
+			return feet;
+		}
+
+		double ConvertKmToMiles(double km)
+		{
+			double miles = 0.621371;
+			miles = km * miles;
+			return miles;
 		}
 	}
 }
